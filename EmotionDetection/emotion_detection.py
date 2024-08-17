bff92ddf6e91554454bdeb6ea905eb121a4334eb
lumosityfan <jeffxie4@gmail.com>
2024-08-17 11:35:43 -0400

    Fixed static analysis errors

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,23 +7,32 @@ def emotion_detector(text_to_analyze):
     input_json = {"raw_document": {"text": text_to_analyze }}
     response = requests.post(url, json = input_json, headers=header)
     formatted_response = json.loads(response.text)
-    emotions = formatted_response["emotionPredictions"][0]["emotion"]
     max_emotion_score = 0
-    max_emotion = "anger"
+    max_emotion = None
+    anger_score = None
+    disgust_score = None
+    fear_score = None
+    joy_score = None
+    sadness_score = None 
     if response.status_code == 400:
-        for emotion in emotions:
-            emotions[emotion] = None
-        max_emotion = None
+        return {'anger': anger_score,
+                'disgust': disgust_score,
+                'fear': fear_score,
+                'joy': joy_score,
+                'sadness': sadness_score,
+                'dominant_emotion': max_emotion
+               }
     elif response.status_code == 200:
+        emotions = formatted_response["emotionPredictions"][0]["emotion"]
         for emotion in emotions:
             if emotions[emotion] > max_emotion_score:
                 max_emotion_score = emotions[emotion]
                 max_emotion = emotion
-    anger_score = emotions["anger"]
-    disgust_score = emotions["disgust"]
-    fear_score = emotions["fear"]
-    joy_score = emotions["joy"]
-    sadness_score = emotions["sadness"]
+        anger_score = emotions["anger"]
+        disgust_score = emotions["disgust"]
+        fear_score = emotions["fear"]
+        joy_score = emotions["joy"]
+        sadness_score = emotions["sadness"]
     return {'anger': anger_score,
             'disgust': disgust_score,
             'fear': fear_score,
--- server.py
+++ server.py
@@ -1,24 +1,28 @@
+""" Module providing implemention of server for emotion detector """
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 
 @app.route('/emotionDetector')
+# Function to run emotion detector when received data from website
 def emot_detector():
+    """ Analyzes text by receiving data from website """
     text_to_analyze = request.args.get('textToAnalyze')
 
     response = emotion_detector(text_to_analyze)
-    if response["dominant_emotion"] == None:
+    if response["dominant_emotion"] is None:
         return "Invalid text! Please try again!"
 
-    return """For the given statement, the system response is 'anger': {}, 'disgust': {}, 
-            'fear': {}, 'joy': {} and 'sadness': {}. 
-            The dominant emotion is {}.""".format(response['anger'], response['disgust'],
-            response['fear'], response['joy'],
-            response['sadness'], response['dominant_emotion']) 
+    return f"""For the given statement, the system response is 'anger':
+              {response['anger']}, 'disgust': {response['disgust']},
+            'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. 
+            The dominant emotion is {response['dominant_emotion']}."""
 
 @app.route('/')
 def render_index_page():
+    ''' Renders index template '''
     return render_template("index.html")
 
 if __name__ == "__main__":
